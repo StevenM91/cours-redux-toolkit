ab2b0258cccfbb29da17744d900159fadbb21b69
Steven Maitre <73316151+StevenM91@users.noreply.github.com>
2022-09-23 16:01:39 +0200

    projet terminer redux toolkit crud complet

--- src/App.js
+++ src/App.js
@@ -2,20 +2,31 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Form from "./components/Form";
 import PicCard from "./components/PicCard";
+import { useDispatch, useSelector } from "react-redux";
+import { setPicturesData } from "./feature/pictures.slice";
 
 const App = () => {
-  const [picsData, setPicsData] = useState([]);
+  // on va chercher les donnée dans la bdd
+  const dispatch = useDispatch();
 
-  useEffect(() => {
+  // on fait apelle au donnée qui est dans le store
+  const picsData = useSelector((state) => state.pictures.pictures);
+
+  // on crée une fonction qui va nous permettre de récuperer l'id directement de la bdd une fois quelque chose de publier
+  const getPictures = () => {
     axios
       .get("http://localhost:5000/pictures")
-      .then((res) => setPicsData(res.data));
+      .then((res) => dispatch(setPicturesData(res.data)));
+  };
+
+  useEffect(() => {
+    getPictures();
   }, []);
 
   return (
     <>
       <h1>NFT Gallery</h1>
-      <Form />
+      <Form getPictures={getPictures} />
       <div className="cards-container">
         {picsData?.map((pic, index) => (
           <PicCard key={index} pic={pic} />
